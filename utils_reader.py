23210a33e4c12da84ef34cec2975899f3af86e3b
BCJuan <jbcarazo@gmail.com>
2019-06-21 00:08:41 +0200

    traintest split

--- utils_reader.py
+++ utils_reader.py
@@ -14,13 +14,17 @@ submission files
 """
 
 from sklearn.base import BaseEstimator, TransformerMixin
-import os
+from os import path, listdir, mkdir
 import pandas as pd
 import urllib.request as req
-import zipfile
-import io
+from zipfile import ZipFile
+from io import BytesIO
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split
+import numpy as np
 
-
+np.random.seed(42)
+test_size = 0.2
 
 class DataFrameSelector(BaseEstimator, TransformerMixin):
     """
@@ -29,11 +33,31 @@ class DataFrameSelector(BaseEstimator, TransformerMixin):
     """
     def __init__(self, attribute_names):
         self.attribute_names = attribute_names
+        
     def fit(self, X, y=None):
         return self
+    
     def transform(self, X):
-        return X[self.attribute_names].values
-
+        return X[self.attribute_names]
+    
+#
+#class official_station_daily_adder(BaseEstimator, TransformerMixin):
+#    
+#    """
+#    Class for adding attributes from official stations to the values predicted 
+#    from the model (real values). The data is the joint for the moment
+#    """
+#    
+#    def __init__(self, attributes_to_add):
+#        self.attributes_to_add = attributes_to_add
+#
+#    def fit(self, X, y=None):
+#        return self
+#    
+#    def transform(self,X, y):
+#        for index, row in X.iterrows():
+#            date = pd.to_datetime(row['day'], format='%Y-%m-%d')
+#            y[y['DATA'] == date]
 
 def read_real_files(direc="./climateChallengeData/real"):
     """
@@ -42,12 +66,15 @@ def read_real_files(direc="./climateChallengeData/real"):
     training val and test division
     """
     files = []
-    for i in os.listdir(direc):
-        name = os.path.join(direc, i)
+    for i in listdir(direc):
+        name = path.join(direc, i)
         files.append(pd.read_csv(name))
     full_file = pd.concat(files)
     return full_file
 
+def read_official_stations(name = "./climateChallengeData/data_S2_S3_S4.xlsx"):
+    return pd.read_excel(name, delimeter=";", sheet_name=[0,1,2,3])
+    
 def download_files(direc="./climateChallengeData/"):
     """
     Downloads files and puts them in a common folder. Dowloads main zip and 
@@ -67,22 +94,22 @@ def download_files(direc="./climateChallengeData/"):
         Saves the sample submissions file inside a scpecific folder in the data 
         folder
         """
-        if not os.path.exists(os.path.join(direc, folder_name)):
-            os.mkdir(os.path.join(direc, folder_name))
-        with open(os.path.join(direc, folder_name, name), 'wb')  as f:
+        if not path.exists(path.join(direc, folder_name)):
+            mkdir(path.join(direc, folder_name))
+        with open(path.join(direc, folder_name, name), 'wb')  as f:
             f.write(content)
             
-    if not os.path.exists(direc):
+    if not path.exists(direc):
         url = "http://climate-challenge.herokuapp.com/climateApp/static/data/climateChallengeData.zip"
         url_sample1 = "http://climate-challenge.herokuapp.com/climateApp/static/sub_example/S1.csv"
         url_sample2 = "http://climate-challenge.herokuapp.com/climateApp/static/sub_example/S2.csv"
         
         print("Downloading \n", end="\r")
 
-        with zipfile.ZipFile(io.BytesIO(give_request(url)), 'r') as z:
+        with ZipFile(BytesIO(give_request(url)), 'r') as z:
 
             print("Extracting data \n", end="\r")
-            os.mkdir(direc)
+            mkdir(direc)
             z.extractall(path=direc)
             
         save_sample(give_request(url_sample1), "S1.csv", direc)
@@ -95,5 +122,52 @@ def download_files(direc="./climateChallengeData/"):
 if __name__ == "__main__":
     
     download_files()
-    read_real_files()
+    full_real = read_real_files()
+    official_stations_daily = read_official_stations()
+    
+    y_columns = ['T_MEAN']
+    x_columns = ['day','LAT','LON']
+    
+    X = DataFrameSelector(x_columns).transform(full_real)
+    y = DataFrameSelector(y_columns).transform(full_real)
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, shuffle=True)
+
+    """
+    
+    """
+    print(official_station_daily[1])
+    
+    #TODO:
+    # make class or fucntion to add official stations values to real values
+    #joining them with date values. FOr example, if I have one row with date x 
+    #I will take the values of day x in official stations and put them as features
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
