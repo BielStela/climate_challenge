cb921eda1242261910148a52d2e591b77382bc37
BCJuan <jbcarazo@gmail.com>
2019-07-14 17:07:30 +0200

    added distance computation

--- utils_reader.py
+++ utils_reader.py
@@ -23,9 +23,18 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 import geopy.distance as distance
 from tqdm import tqdm as tqdm
+import argparse
 
 np.random.seed(42)
 test_size = 0.2
+
+
+def parse_arguments(parser):
+    parser.add_argument("-d", dest="comp_dist",
+                        help="compute distances and save them in a file",
+                        type=int,
+                        default=0)
+    return parser.parse_args()
 
 
 class DataFrameSelector(BaseEstimator, TransformerMixin):
@@ -62,8 +71,9 @@ class official_station_daily_adder(BaseEstimator, TransformerMixin):
     def transform(self, X, y):
         X['day'] = pd.to_datetime(X['day'])
         self.full = X.copy()
-        for i, j in zip(self.attributes_to_add,
-                        range(len(self.attributes_to_add))):
+        for i, j in tqdm(
+                zip(self.attributes_to_add, range(len(self.attributes_to_add))), 
+                total=len(self.attributes_to_add)):
             self.full = pd.merge(self.full, y[j][i], how='inner',
                                  left_on='day', right_on='DATA',
                                  suffixes=("_" + str(j-1), "_" + str(j)))
@@ -105,6 +115,23 @@ def read_real_files(direc="./climateChallengeData/real"):
 
 def read_official_stations(name="./climateChallengeData/data_S2_S3_S4.xlsx"):
     return pd.read_excel(name, delimeter=";", sheet_name=[0, 1, 2, 3])
+
+
+def compute_distances(latLonStations, gridPoints,
+                      file_name="./climateChallengeData/distances.csv"):
+
+    for i in range(len(latLonStations)):
+        gridPoints['dist'+str(i)] = gridPoints.apply(
+                lambda x: distance.geodesic(
+                        (x['LAT'], x['LON']),
+                        (latLonStations.loc[i, 'Latitude'],
+                         latLonStations.loc[i, 'Longitude'])).km,
+                axis=1)
+    gridPoints.to_csv(file_name)
+
+
+def create_idx(df):
+    df['nxny'] = df.apply(lambda x: str(int(x['nx'])).strip() + str(int(x['ny'])).strip(), axis=1)
 
 
 def download_files(direc="./climateChallengeData/"):
@@ -153,16 +180,27 @@ def download_files(direc="./climateChallengeData/"):
 
 if __name__ == "__main__":
 
+    parser = argparse.ArgumentParser()
+    parsed = parse_arguments(parser)
+
     download_files()
     full_real = read_real_files()
     official_stations_daily = read_official_stations()
     official_stations_latlon = pd.read_csv("./climateChallengeData/officialStations.csv")
+    grid_points = pd.read_csv("./climateChallengeData/grid2latlon.csv")
 
     y_columns = ['T_MEAN']
-    x_columns = ['day', 'LAT', 'LON']
+    x_columns = ['day', 'ny', 'nx']
 
     X = DataFrameSelector(x_columns).transform(full_real)
     y = DataFrameSelector(y_columns).transform(full_real)
+
+    create_idx(X)
+    create_idx(grid_points)
+    create_idx(official_stations_latlon)
+
+    if parsed.comp_dist:
+        compute_distances(official_stations_latlon, grid_points)
 
     X_train, X_test, y_train, y_test = train_test_split(X, y,
                                                         test_size=test_size,
@@ -175,7 +213,8 @@ if __name__ == "__main__":
 
     X_train_complete = official_station_daily_adder(
             official_attr,
-            official_stations_latlon).transform(
+            official_stations_latlon,
+            include_distance=False).transform(
             X_train, official_stations_daily)
 
     print(X_train_complete.head(20))
