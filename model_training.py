457e17d9b61ecef7241f9fe4f1540ce6b887d06a
BCJuan <jbcarazo@gmail.com>
2019-09-02 08:50:22 +0200

    first model implementations

--- model_training.py
+++ model_training.py
@@ -12,11 +12,14 @@ from sklearn.model_selection import KFold
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_squared_error
-from pandas import read_csv
+from pandas import read_csv, DataFrame
 from math import sqrt
+import numpy as np
+from os import path
 
 
 def load_data():
+
     scaler = StandardScaler()
 
     X = read_csv("./data_for_models/X.csv").values
@@ -25,20 +28,50 @@ def load_data():
     scaler.fit(X)
     X_std = scaler.transform(X)
 
-    return X_std, y
+    return X_std, y, X.columns
 
 
-if __name__ == "__main__":
-    load_data()
-    
+def save_results(file, name, score, variables, index):
+
+    file[index] = [name, score, variables]
+    file.to_csv("results.csv")
+    return file
+
+
+def load_results(name="./results.csv"):
+    if path.exists(name):
+        df = read_csv(name)
+        index = len(df)
+        return df, index
+    else:
+        df = DataFrame(columns=['name', 'score', 'variables'])
+        return df, 0
+
+
+def random_forest_default(X, y, df, variables, i):
+
+    results = []
     fold = KFold(n_splits=5, shuffle=True)
 
-    for train_idx, test_idx in fold.split(X_std):
-        X_train, X_test = X_std[train_idx], X_std[test_idx]
+    for train_idx, test_idx in fold.split(X):
+        X_train, X_test = X[train_idx], X[test_idx]
         y_train, y_test = y[train_idx], y[test_idx]
 
         forest = RandomForestRegressor(n_jobs=-1)
         forest.fit(X_train, y_train)
         y_pred = forest.predict(X_test)
 
-        print(sqrt(mean_squared_error(y_test, y_pred)))
+        results.append(sqrt(mean_squared_error(y_test, y_pred)))
+
+    df = save_results(df, "random_forest_default", np.mean(results), variables,
+                      i)
+
+    return df, i + 1
+
+
+if __name__ == "__main__":
+
+    X, y, variables = load_data()
+    df, i = load_results()
+
+    df, i = random_forest_default(X, y, df,  variables, i)
